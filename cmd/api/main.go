203162b71b7cfacb3c2070dc79bd6eb0ba38144f
Puneet Kumar Mishra <puneetmishrajpr17@gmail.com>
2026-01-26 19:44:42 +0530

    Added Json response format to healthcheck handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -25,7 +25,7 @@ func main() {
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Enviroment (development|staging|production)")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
