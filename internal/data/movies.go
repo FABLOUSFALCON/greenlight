2532aad5d037383a5ceeda68c485817417aab8d2
Puneet Kumar Mishra <puneetmishrajpr17@gmail.com>
2026-01-30 18:27:46 +0530

    Formatted and Enveloped responses

---
+++ internal/data/movies.go
@@ -0,0 +1,15 @@
+// Package data stores all the custom data types and logic to handle DB
+package data
+
+import "time"
+
+// Movie holds the information of a particular movie in our DB
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitzero"`
+	Runtime   int32     `json:"runtime,omitzero"`
+	Genres    []string  `json:"genres,omitzero"`
+	Version   int32     `json:"version"`
+}
